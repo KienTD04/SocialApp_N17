a4f7a2e48b492ec3a44590b6a42ad1a88137b1d7
Kien <duckien05052004@gmail.com>
2025-12-03 19:44:38 +0700

    Initial comit

---
+++ app/src/main/java/com/example/socialapp/adapter/ViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.socialapp.adapter;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.annotation.NonNull;
+
+import com.example.socialapp.fragments.Add;
+import com.example.socialapp.fragments.Home;
+import com.example.socialapp.fragments.Notification;
+import com.example.socialapp.fragments.Profile;
+import com.example.socialapp.fragments.Search;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    int noOfTabs;
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int noOfTabs){
+        super(fm);
+        this.noOfTabs = noOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position){
+
+        switch (position){
+            case 0:
+                return new Home();
+            case 1:
+                return new Search();
+            case 2:
+                return new Add();
+            case 3:
+                return new Notification();
+            case 4:
+                return new Profile();
+            default:
+                return null;
+        }
+
+    }
+    @Override
+    public int getCount(){
+        return noOfTabs;
+    }
+}
